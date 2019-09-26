3f7c050d3053d20190b5819a8c190f8d9aaef3f2
Wanderson David <wandersondavid.ifg@gmail.com>
2019-09-26 09:35:39 -0300

    correção

--- conversor_react/src/conponents/Conversor.js
+++ conversor_react/src/conponents/Conversor.js
@@ -12,14 +12,14 @@ export default class Conversor extends Component{
  }
  converter(){
      let de_para = `${this.props.moedaA}_${this.props.moedaB}`
-     let url = ``
+     let url = `http://free.currencyconverterapi.com/api/v5/convert?q=${de_para}&compact=y`
  fetch(url)
  .then(res =>{
      return res.json()
  })
- .then(res =>{
+ .then(json =>{
      let cotacao = json[de_para].val;
-     let moedaB_valor = (parseFloat(this))
+     let moedaB_valor = (parseFloat(this.state.moedaA_valor) * cotacao).toFixed(2)
      this.setState({moedaB_valor})
  })
 // console.log("teste")
@@ -31,7 +31,7 @@ export default class Conversor extends Component{
                 <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
                 <input type="text" onChange={(event)=>{this.setState({moedaA_valor:event.target.value})}}></input>
                 <input type="button" value="Converter" onClick={this.converter}></input>
-                <h2>valor convertido</h2>
+                <h2>{this.state.moedaB_valor}</h2>
             </div>
         )
     }
